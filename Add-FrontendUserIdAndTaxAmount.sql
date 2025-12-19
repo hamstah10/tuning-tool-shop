a5759988bd668683b0007001c0d4ddf37a9d42b4
hamstah10 <hamstahstudio@gmail.com>
2025-12-19 19:26:33 +0000

    Update

--- Add-FrontendUserIdAndTaxAmount.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE tx_tuningtoolshop_domain_model_order ADD COLUMN IF NOT EXISTS frontend_user_id INT(11) DEFAULT 0;
-ALTER TABLE tx_tuningtoolshop_domain_model_order ADD COLUMN IF NOT EXISTS tax_amount DECIMAL(11,2) DEFAULT 0.00;
